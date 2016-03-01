c3ddca189be7d38fb51eef580991c173c8629dc4
kanaf34 <kanaf34@gmail.com>
2016-03-01 22:54:46 +0900

    google app script 와 연결 - jsonp 로 oricon 차트 데이터 받음

--- app/main.js
+++ app/main.js
@@ -1,14 +1,40 @@
-var cheerio = require('cheerio')
-  , request = require('request');
+var loadJSONP = (function() {
+	var unique = 0;
+	return function(url, callback, context) {
+		var name = "_jsonp_" + unique++;
+		if (url.match(/\?/)) url += "&callback="+name;
+    	else url += "?callback="+name;
 
-console.log('1');
+		var script = document.createElement('script');
+		script.type = 'application/javascript';
+		script.src  = url;
 
-request(
-	'http://www.oricon.co.jp/rank/js/d/2016-02-26/'
-	, function(error, response, body) {
-		console.log('2');
-		if (!error && response.statusCode == 200) {
-			console.log(body);
+		window[name] = function(data) {
+			callback.call((context||window), data);
+			document.getElementsByTagName('head')[0].removeChild(script);
+			script = null;
+			delete window[name];
 		}
+
+		document.getElementsByTagName('head')[0].appendChild(script);
 	}
-)
+})();
+
+var parsePageToJson = function(result) {
+	var divRank = document.getElementById('rankList');
+
+	var rankList = result.rankList;
+	var ul = document.createElement('ul');
+	for (var i=0; i<rankList.length; i++) {
+		var li = document.createElement('li');
+		li.innerText = rankList[i].title + ' / ' + rankList[i].singer + ' / ' + rankList[i].organization;
+		ul.appendChild(li);
+	}
+	divRank.appendChild(ul);
+}
+
+
+var url = 'https://script.google.com/macros/s/AKfycbx_JQBUfwIjhR2lc_9xx3ByPsyXiZNA9RpIVdCcZ7xEGITEiIQj/exec';
+url += '?action=singleDaily'
+
+loadJSONP(url, parsePageToJson, this);
